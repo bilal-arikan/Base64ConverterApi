8734981da5423e0c267cedc4833fbaeb6d88c357
Bilal Arikan <bilal1993arikan@gmail.com>
2024-11-05 16:03:27 +0300

    WIP deneme

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   const url_file = URIHash.get("file");
   if (url_file) {
     console.log(`Load file from file`);
-    atob(url_file);
+    document.body.innerHTML = atob(url_file);
   }
+  document.body.innerHTML = "";
 });
